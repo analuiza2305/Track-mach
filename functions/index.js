f8ea39c23b383b425ae969b33b014cdf06235820
frnzdev <felipeacfornazeiro@gmail.com>
2025-08-27 10:59:44 -0300

    first commit

--- functions/index.js
+++
@@ -1,9 +0,0 @@
-// functions/index.js
-const {onRequest} = require("firebase-functions/v2/https");
-const {setGlobalOptions} = require("firebase-functions/v2");
-
-// opcional: região do Brasil para menor latência
-setGlobalOptions({region: "southamerica-east1", maxInstances: 5});
-
-const app = require("./api"); // importa seu Express
-exports.api = onRequest(app); // expõe /api como Function HTTP
